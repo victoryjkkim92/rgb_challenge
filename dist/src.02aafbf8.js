d0f3e4023a5654e8cc61ffcbad0502735444a84f
jukyung kim <victoryjkkim92@gmail.com>
2018-10-23 19:05:26 +0900

    Feat : somefiles've been modified

--- dist/src.02aafbf8.js
+++ dist/src.02aafbf8.js
@@ -165,7 +165,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":4}],6:[function(require,module,exports) {
+},{"_css_loader":4}],11:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -194,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53390' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '55988' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -335,4 +335,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[6], null)
+},{}]},{},[11], null)
